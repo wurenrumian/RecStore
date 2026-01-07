9265a0eb36513ae054e100fe39af6ca0c8bf78d5
choimoe <qwq.shq@gmail.com>
2026-01-07 00:23:22 +0000

    refactor: remove unused DGL-related modules and files

--- src/kg/kg/diff.sh
+++
@@ -1,3 +0,0 @@
-cd dgl-ke
-git diff >../dgl-ke.patch
-
--- src/kg/scripts/variables.py
+++
@@ -1,7 +0,0 @@
-PROJECT_PATH = "/home/xieminhui/RecStore/"
-
-LOG_PREFIX = f'{PROJECT_PATH}/log'
-
-ALL_SERVERS_INCLUDING_NOT_USED = [
-    '127.0.0.1',
-]
