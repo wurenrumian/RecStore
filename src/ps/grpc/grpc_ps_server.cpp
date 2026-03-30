f9f46f3aedc4f496f1843e7199d1c03cdff484b0
Choimoe <qwq.shq@gmail.com>
2026-03-30 16:04:02 +0800

    fix(perf): improve memory allocation error handling in ExtendibleHash

--- src/ps/grpc/grpc_ps_server.cpp
+++ src/ps/grpc/grpc_ps_server.cpp
@@ -321,7 +321,9 @@ private:
               request->gradients().data());
       size = reader->item_size();
 
+    #ifdef ENABLE_PERF_REPORT
       before_cache_update_time = std::chrono::high_resolution_clock::now();
+    #endif
       success = cache_ps_->UpdateParameter(table_name, reader, 0);
 
       FB_LOG_EVERY_MS(INFO, 2000)
